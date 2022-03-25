a40cdf35cffbd4432e605680518e234b0dbfd427
Anil-PragmaEdge <93245991+Anil-PragmaEdge@users.noreply.github.com>
2022-03-25 17:12:07 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,4 +5,34 @@ output "instance_ami" {
 output "instance_arn" {
   value = aws_instance.ubuntu.arn
 }
-
+output "server_ip_address" {
+  value = aws_eip.nateip.address
+}
+output "vpc_id" {
+  value = aws_vpc.automation_vpc.id
+}
+output "vpc_security_group_id" {
+  value = aws_vpc.automation_vpc.default_security_group_id
+}
+output "public_subnet" {
+  value = aws_subnet.pubsub.id
+}
+output "private_subnet" {
+  value = aws_subnet.privsub.id
+}
+output "database_name" {
+  value = aws_db_instance.automation_db.name
+}
+output "database_username" {
+  value = aws_db_instance.automation_db.username
+}
+output "database_password" {
+  value     = aws_db_instance.automation_db.password
+  sensitive = true
+}
+output "database_endpoint" {
+  value = aws_db_instance.automation_db.endpoint
+}
+output "database_port" {
+  value = aws_db_instance.automation_db.port
+}
