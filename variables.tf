712a4bc77193e9e0b07bdae24c3bce8c785a269b
Anil-PragmaEdge <93245991+Anil-PragmaEdge@users.noreply.github.com>
2022-03-25 17:11:30 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,4 +12,79 @@ variable "instance_name" {
   description = "EC2 instance name"
   default     = "Provisioned by Terraform"
 }
-
+variable "vpc_cidr_block" {
+}
+variable "vpc_name" {
+}
+variable "public_subnet_name" {
+}
+variable "public_cidr_block" {
+}
+variable "assign_ipv6_address_on_creation_pubsub" {
+}
+variable "availability_zone_pubsub" {
+}
+variable "private_subnet_name" {
+}
+variable "private_cidr_block" {
+}
+variable "elastic_ip_name" {
+}
+variable "security_group_name" {
+}
+variable "assign_ipv6_address_on_creation_prvsub" {
+}
+variable "availability_zone_prvsub" {
+}
+variable "associate_public_ipaddress_for_instance" {
+}
+variable "tenancy" {
+}
+variable "monitoring" {
+}
+variable "root_volume_size" {
+}
+variable "root_volume_type" {
+}
+variable "db_instance_class" {
+}
+variable "allocated_storage" {
+}
+variable "auto_minor_version_upgrade" {
+}
+variable "backup_retention_period" {
+}
+variable "availability_zone_instance" {
+}
+variable "publicly_accessible" {
+}
+variable "performance_insights_enabled" {
+}
+variable "performance_insights_retention_period" {
+}
+variable "engine" {
+}
+variable "engine_version" {
+}
+variable "database_name" {
+}
+variable "database_username" {
+}
+variable "database_password" {
+}
+variable "storage_encrypted" {
+}
+variable "storage_type" {
+}
+variable "skip_final_snapshot" {
+}
+variable "database_port" {
+}
+variable "iops" {
+}
+variable "maintenance_window" {
+}
+variable "database-instance-identifier" {
+}
+variable "delete_rbs_on_termination" {
+}
